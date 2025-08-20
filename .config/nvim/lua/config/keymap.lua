a903656f98eecb0df25477fce8b80f6d105451d9
Antoni Grabowski <grabowski.antoni@proton.me>
2025-08-20 15:49:39 +0200

    Add lazydev etc.

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,5 +1,4 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.keymap.set('n', '<Esc>', '<cmd>nohl<CR>')
-vim.keymap.set('n', '<leader>pv', '<cmd>Ex<CR>')
-
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.keymap.set("n", "<Esc>", "<cmd>nohl<CR>")
+vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,22 +1,50 @@
-return{
-	'hrsh7th/nvim-cmp',
-	dependencies ={
-		'neovim/nvim-lspconfig',
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-path',
-		'hrsh7th/cmp-cmdline'
+return {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"folke/lazydev.nvim",
+		-- Snippet Engine
+		{
+			"L3MON4D3/LuaSnip",
+			version = "2.*",
+			build = (function()
+				-- Build Step is needed for regex support in snippets.
+				-- This step is not supported in many windows environments.
+				-- Remove the below condition to re-enable on windows.
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				-- `friendly-snippets` contains a variety of premade snippets.
+				--    See the README about individual language/framework/plugin snippets:
+				--    https://github.com/rafamadriz/friendly-snippets
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
+			},
+			opts = {},
+		},
+		"saadparwaiz1/cmp_luasnip",
 	},
 	config = function()
 		-- Set up nvim-cmp.
-		local cmp = require'cmp'
+		local cmp = require("cmp")
 
 		cmp.setup({
 			snippet = {
 				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
-					vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-					-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 					-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
@@ -33,21 +61,21 @@ return{
 				-- documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
-				['<C-b>'] = cmp.mapping.scroll_docs(-4),
-				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete(),
-				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			}),
 			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' },
-				{ name = 'vsnip' }, -- For vsnip users.
-				-- { name = 'luasnip' }, -- For luasnip users.
+				{ name = "nvim_lsp" },
+				-- { name = "vsnip" }, -- For vsnip users.
+				{ name = "luasnip" }, -- For luasnip users.
 				-- { name = 'ultisnips' }, -- For ultisnips users.
 				-- { name = 'snippy' }, -- For snippy users.
 			}, {
-				{ name = 'buffer' },
-			})
+				{ name = "buffer" },
+			}),
 		})
 
 		-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
@@ -59,29 +87,30 @@ return{
 				{ name = 'buffer' },
 			})
 		})
-		require("cmp_git").setup() ]]-- 
+		require("cmp_git").setup() ]]
+		--
 
 		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-		cmp.setup.cmdline({ '/', '?' }, {
+		cmp.setup.cmdline({ "/", "?" }, {
 			mapping = cmp.mapping.preset.cmdline(),
 			sources = {
-				{ name = 'buffer' }
-			}
+				{ name = "buffer" },
+			},
 		})
 
 		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-		cmp.setup.cmdline(':', {
+		cmp.setup.cmdline(":", {
 			mapping = cmp.mapping.preset.cmdline(),
 			sources = cmp.config.sources({
-				{ name = 'path' }
+				{ name = "path" },
 			}, {
-				{ name = 'cmdline' }
+				{ name = "cmdline" },
 			}),
-			matching = { disallow_symbol_nonprefix_matching = false }
+			matching = { disallow_symbol_nonprefix_matching = false },
 		})
 
 		-- Set up lspconfig.
-		local capabilities = require('cmp_nvim_lsp').default_capabilities()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 		-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 		-- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
 		-- 	capabilities = capabilities
@@ -89,5 +118,12 @@ return{
 		-- require('mason-lspconfig').setup {
 		-- 	capabilities = capabilities
 		-- }
-	end
-}	
+	end,
+	-- opts = function(_, opts)
+	-- 	opts.sources = opts.sources or {}
+	-- 	table.insert(opts.sources, {
+	-- 		name = "lazydev",
+	-- 		group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+	-- 	})
+	-- end,
+}
