1cccd02355828e44d948f0f3051214236be811e8
Antoni Grabowski <grabowski.antoni@proton.me>
2025-08-21 17:32:11 +0200

    Add treesitter configuration

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -119,11 +119,11 @@ return {
 		-- 	capabilities = capabilities
 		-- }
 	end,
-	-- opts = function(_, opts)
-	-- 	opts.sources = opts.sources or {}
-	-- 	table.insert(opts.sources, {
-	-- 		name = "lazydev",
-	-- 		group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-	-- 	})
-	-- end,
+	opts = function(_, opts)
+		opts.sources = opts.sources or {}
+		table.insert(opts.sources, {
+			name = "lazydev",
+			group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+		})
+	end,
 }
