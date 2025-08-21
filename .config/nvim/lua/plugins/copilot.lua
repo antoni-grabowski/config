2ced010efa42986a8fa9e30d7e600ccf988679cc
Antoni Grabowski <grabowski.antoni@proton.me>
2025-08-22 00:20:23 +0200

    Add copilot.lua.

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,13 +1,8 @@
 return {
 	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
 	config = function()
-		require("copilot").setup({
-			suggestion = {
-				auto_trigger = true,
-			},
-			keymap = {
-				accept = "<C-l>",
-			},
-		})
+		require("copilot").setup(options)
 	end,
 }
