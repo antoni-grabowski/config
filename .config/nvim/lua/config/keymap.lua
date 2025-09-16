16483549ff5a5d5e7228000aec5c1527e55d2e72
Antoni Grabowski <grabowski.antoni@proton.me>
2025-09-16 19:33:34 +0200

    Complete nvim config

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
 vim.keymap.set("n", "<ESC>", "<cmd>nohl<CR>")
+
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,5 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.breakindent = true
 vim.o.undofile = true
+vim.o.number = true
+vim.o.relativenumber = true
