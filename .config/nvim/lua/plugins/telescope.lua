bf0ea0d15aeff5dc4dcfe947a33273cce7294777
Antoni Grabowski <grabowski.antoni@proton.me>
2025-10-29 18:50:48 +0100

    Add new colorscheme

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,8 @@
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		telescope = require('telescope.builtin')
+		vim.keymap.set('n', '<leader><leader>', telescope.find_files)
+	end
 }
